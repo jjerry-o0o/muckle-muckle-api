41f8e19733650a1c0adc2fb561fbf6ee92118184
jjerry-o0o <alfndp25@gmail.com>
2026-02-27 09:58:01 +0900

    init commit

---
+++ src/main/java/com/future/micklemuckle/MickleMuckleApiApplication.java
@@ -0,0 +1,13 @@
+package com.future.micklemuckle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MickleMuckleApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MickleMuckleApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/future/micklemuckle/MickleMuckleApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.future.micklemuckle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MickleMuckleApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
