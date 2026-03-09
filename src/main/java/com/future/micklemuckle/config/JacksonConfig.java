3d5d9dfeb576f2619fb565ec89af3a027164cd0d
jjerry-o0o <alfndp25@gmail.com>
2026-03-09 11:42:32 +0900

    chore: 스프링부트 3버전으로 낮추고, JacksonConfig 파일 추가 - 스프링부트 4는 Jackson 3버전 사용 / JsonNullable은 Jackson 2버전 사용하고 있어서 충돌남.

--- src/main/java/com/future/micklemuckle/config/JacksonConfig.java
+++ src/main/java/com/future/micklemuckle/config/JacksonConfig.java
@@ -1,11 +1,5 @@
 package com.future.micklemuckle.config;
 
-
-import com.fasterxml.jackson.annotation.JsonInclude;
-import com.fasterxml.jackson.databind.DeserializationFeature;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.SerializationFeature;
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.openapitools.jackson.nullable.JsonNullableModule;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -19,12 +13,8 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class JacksonConfig {
 
-    @Bean("objectMapper")
-    public ObjectMapper objectMapper() {
-        return new ObjectMapper()
-                .setSerializationInclusion(JsonInclude.Include.ALWAYS)
-                .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
-                .disable(SerializationFeature.FAIL_ON_EMPTY_BEANS, SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)
-                .registerModules(new JsonNullableModule(), new JavaTimeModule());
+    @Bean
+    public JsonNullableModule jsonNullableModule() {
+        return new JsonNullableModule();
     }
 }
