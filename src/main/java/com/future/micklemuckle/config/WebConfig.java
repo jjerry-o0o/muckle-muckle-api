b848707ba24ddd78746a3759acd294ffb7e19972
jjerry-o0o <alfndp25@gmail.com>
2026-04-01 17:28:11 +0900

    chore: CORS 설정 수정

--- src/main/java/com/future/micklemuckle/config/WebConfig.java
+++ src/main/java/com/future/micklemuckle/config/WebConfig.java
@@ -11,8 +11,9 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins("http://localhost:5173")
-                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
                 .allowCredentials(true)
+                .allowedHeaders("*")
                 .maxAge(3600);
     }
 }
