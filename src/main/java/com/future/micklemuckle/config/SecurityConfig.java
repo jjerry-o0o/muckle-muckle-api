3e800f674752a82024a92f3917f7917da04731db
jjerry-o0o <alfndp25@gmail.com>
2026-06-18 16:56:25 +0900

    feat: Spring Security 에 cors 설정 추가

--- src/main/java/com/future/micklemuckle/config/SecurityConfig.java
+++ src/main/java/com/future/micklemuckle/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.future.micklemuckle.config;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -22,6 +23,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         return http
+                .cors(Customizer.withDefaults())
                 .csrf(AbstractHttpConfigurer::disable)
                 .sessionManagement(session ->
                         session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
