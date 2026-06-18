320c75258bf0d0669ca403e09c384cd4357e25c4
jjerry-o0o <alfndp25@gmail.com>
2026-06-18 09:40:38 +0900

    feat: JWT 인증 기반 설정 추가 (JwtProvider, JwtFilter, SecurityConfig)

---
+++ src/main/java/com/future/micklemuckle/modules/user/entity/User.java
@@ -0,0 +1,36 @@
+package com.future.micklemuckle.modules.user.entity;
+
+import com.future.micklemuckle.common.entity.BaseTimeEntity;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "users")
+public class User extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
+    private Long id;
+
+    @Column(name = "user_email", unique = true)
+    private String email;
+
+    @Column(name = "user_password")
+    private String password;
+
+    @Column(name = "user_nickname", unique = true)
+    private String nickname;
+
+    @Builder
+    public User(String email, String password, String nickname) {
+        this.email = email;
+        this.password = password;
+        this.nickname = nickname;
+    }
+}
